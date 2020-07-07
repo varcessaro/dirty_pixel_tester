8fc9eda7b230d640cbb113b88eabaeb754e48fcc
varcessaro <66271718+varcessaro@users.noreply.github.com>
2020-07-07 11:24:32 -0400

    Updated main.c

--- src/main.c
+++ src/main.c
@@ -1,62 +1,46 @@
 #include <stdio.h>
 #include <stdint.h>
-#include "dpc_core.h"
+#include <string.h>
+#include "kmz_core.h"
+#include "kmz_gd_2x_image_file.h"
 #include "dpc_contrast.h"
-#include "dpc_gdimage.h"
 
 int main(int argc, const char * argv[]) {
-    dpc_file f = NULL;
-    if (2 == argc) {
-        f = fopen(argv[1], "wb+");
+    FILE * f = NULL;
+    FILE * o = NULL;
+    if (1 < argc) {
+        f = fopen(argv[1], "rb+");
     } else {
         f = stdin;
     }
-    
-    if (f) {
-        struct dpc_gd_2x_header_t header;
-        dpc_byte result;
-        
-        if ((result = dpc_read_gd_2x_header(f, &header))) {
-            struct dpc_gd_2x_header_t output_header = {
-                .s={
-                    .signature=header.s.signature,
-                    .width=header.s.width,
-                    .height=header.s.height,
-                },
-                .c={
-                    .is_truecolor=0,
-                    .header={.truecolor={.transparent=4294967295},},
-                },
-            };
-            dpc_color_32 * body = calloc(header.s.width * header.s.height, sizeof(dpc_color_32));
-            dpc_color_32 * output = calloc(header.s.width * header.s.height, sizeof(dpc_color_32));
-            if ((result = dpc_read_gd_2x_bytes(f, &header, body))) {
-                if (dpc_perform_contrast(header.s.width, header.s.height, 5, &header, body, output)) {
-                    dpc_file o = NULL;
-                    if (f == stdin) {
-                        o = stdout;
-                    } else {
-                        o = f;
-                    }
-                    if (o) {
-                        if ((result = dpc_write_gd_2x_header(o, &output_header))) {
-                            result = dpc_write_gd_2x_bytes(o, &output_header, output);
-                        }
-                        fclose(o);
-                    }
-                }
-            }
-            free(body);
-            free(output);
-        }
-        fclose(f);
-        if (1 == result) {
-            return 0;
-        } else {
-            return result + 1;
-        }
+    if (2 < argc) {
+        o = fopen(argv[2], "wb+");
     } else {
-        // ERROR: Failed to load image
-        return 1;
+        o = stdout;
     }
+    
+    KmzGd2xImageFile img = {};
+    kmz_gd_2x_image_file_status status = kmz_read_gd_2x_image_file(f, &img);
+    if (status != OK) {
+        fputs(kmz_status_msg(status), stderr);
+        fclose(f);
+        fclose(o);
+        return (int)status;
+    }
+    
+    KmzImage * w = kmz_make_image_from_gd_2x(img);
+    dpc_perform_contrast(w);
+    memcpy(img.pixels, w->pixels, w->len * sizeof(kmz_color_32));
+    
+    status = kmz_write_gd_2x_image_file(o, &img);
+    if (status != OK) {
+        fputs(kmz_status_msg(status), stderr);
+    }
+    fclose(f);
+    fclose(o);
+    free(w->pixels);
+    free(w);
+    free(img.pixels);
+    return (int)status;
 }
+
